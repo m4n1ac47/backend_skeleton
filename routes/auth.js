b8b986f3aa53f55149d79d1532b6772160ea6e70
m4n1ac47 <m4n1ac47@gmail.com>
2021-02-18 18:10:14 +0300

    rewrite auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,31 +5,34 @@ const jwt = require('jsonwebtoken')
 const { User } = require('../models/user.js')
 const { PRIVATE_KEY } = require('../config.js')
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   const newUser = new User(req.body)
 
-  newUser.hash_password = bcrypt.hashSync(req.body.password, 10)
+  try {
+    newUser.hash_password = await bcrypt.hash(req.body.password, 10)
 
-  newUser.save((err, user) => {
-    if (err) {
-      return res.status(400).send({ message: err })
-    } else {
-      user.hash_password = undefined
-      return res.json(user)
-    }
-  })
+    const user = await newUser.save()
+
+    user.hash_password = undefined
+
+    return res.json(user)
+  } catch (err) {
+    return res.status(400).send({ message: err })
+  }
 })
 
-router.post('/sign_in',  (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (err) throw err
+router.post('/sign_in',  async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    const failAuth = !user || !user.comparePassword(req.body.password)
 
-    if (!user || !user.comparePassword(req.body.password)) {
-      return res.status(400).json({ message: 'Authentication failed. Invalid user or password.' })
-    }
+    return failAuth
+      ? res.status(400).json({ message: 'Authentication failed. Invalid user or password.' })
+      : res.json({ token: jwt.sign({ email: user.email, name: user.name, _id: user._id }, PRIVATE_KEY) })
 
-    return res.json({ token: jwt.sign({ email: user.email, name: user.name, _id: user._id }, PRIVATE_KEY) })
-  })
+  } catch (err) {
+    if (err) return res.status(400).json({ message: err })
+  }
 })
 
 router.post('/tasks', loginRequired, profile)
@@ -42,13 +45,10 @@ function loginRequired(req, res, next) {
   }
 }
 
-function profile(req, res, next) {
-  if (req.user) {
-    return res.json(req.user);
-  }
-  else {
-   return res.status(401).json({ message: 'Invalid token' });
-  }
+function profile(req, res) {
+  return req.user
+    ? res.json(req.user)
+    : res.status(401).json({ message: 'Invalid token' })
 }
 
 module.exports = router
